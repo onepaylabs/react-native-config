f81c620045f5410435f9296c2aeea32d0db15d98
Junzhen Wu <zhenwusw@gmail.com>
2017-08-30 11:35:04 +0800

    fix ReactNativeConfigPacke should override createJSModule complaint

--- android/src/main/java/com/lugg/ReactNativeConfig/ReactNativeConfigPackage.java
+++ android/src/main/java/com/lugg/ReactNativeConfig/ReactNativeConfigPackage.java
@@ -22,4 +22,9 @@ public class ReactNativeConfigPackage implements ReactPackage {
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
     }
+
+    @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
 }
